319eb9e4173dfcaac3399e8760fe54f00f619e56
shakti7 <shaktiprasanna.sahoo.7@gmail.com>
2024-11-15 00:45:57 +0530

    middleware and route handler

--- src/app.js
+++ src/app.js
@@ -2,26 +2,30 @@ const express = require('express');
 
 const app = express();
 
-app.use('/user',[(req,res,next)=>{
-    console.log('Handling the route user ');
-    next();
-    // res.send('Re sponse!!!')
-},(req,res ,next)=>{
-    console.log('Handling the route user 2');
-    // res.send('2nd Response!!!')
-    next ();
-}], (req,res,next)=> {
-    
-    console.log('Handling the route user 3');
-    // res.send('3rd Response!!!')
+
+app.use('/',(req,res,next)=>{
     next()
-},(req,res,next)=> {
-    
-    console.log('Handling the route user 4');
-    res.send('4th Response!!!')
-    // next()
+    // res.send("Handling / route")
+})
+
+// app.use('/test/:user',(req,res)=>{
+//     console.log(req.params);
+//     res.send("Hi")
+// })
+
+app.use('/lunch',(req,res,next)=>{
+    console.log("handling lunch router");
+    res.send("lunch")
+})
+app.use('/user',( req,res,next)=>{
+    console.log('Handling the route user ');
+    next()
 }
 )
+app.use('/user',(req,res,next)=>{
+    console.log("handling 2nd router");
+    res.send("2nd Route")
+})
 
 app.listen(7777,()=>{
     console.log("Server is successfully listening on Port 7777...");
